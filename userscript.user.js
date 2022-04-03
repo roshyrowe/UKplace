be67641313a8e2fd7ba1552d9de32797223a21f1
og2701 <67282414+og2701@users.noreply.github.com>
2022-04-03 15:02:51 +0100

    Version 11

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Union Flag Bot
 // @namespace
-// @version      10.0
+// @version      11.0
 // @description  For Britannia!
 // @author       Union Flag Project
 // @match        https://www.reddit.com/r/place/*
