ec574b56c00a6085c777dde9068ea70162e74b08
Alex <40502404+AlexAndHisScripts@users.noreply.github.com>
2022-04-03 20:50:57 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -181,9 +181,7 @@ async function attemptPlace() {
         return;
     }
 
-    const rgbaOrder = currentOrderCtx.getImageData(0, 0, 2000, 1000).data;
-    const rgbaCanvas = ctx.getImageData(0, 0, 2000, 1000).data;
-        const rgbaOrder = currentOrderCtx.getImageData(0, 0, 2000, 2000).data;
+    const rgbaOrder = currentOrderCtx.getImageData(0, 0, 2000, 2000).data;
     const rgbaCanvas = ctx.getImageData(0, 0, 2000, 2000).data;
     const work = getPendingWork(order, rgbaOrder, rgbaCanvas);
 
